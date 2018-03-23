bf420f264057b261c6fc1cb38f89b131fef93f3e
Dennis Sivia <dennis.sivia@gmail.com>
2018-03-23 07:34:17 +0100

    Example how to read data from a dht11

---
+++ experiments/dht11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
+import time
+import requests
+import json
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    data = {'humidity':humidity,'temperature':temperature}
+    print(humidity,temperature)
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    time.sleep(2)
